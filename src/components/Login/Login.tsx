aaadaf5c6415e04b99564ed29a3e9612b85d7a88
aokisinn <72698168+aokisinn@users.noreply.github.com>
2022-04-03 17:27:26 +0900

    リファクタリング

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,94 +1,9 @@
-import { useState, useEffect } from "react";
-import { supabase } from "@/utils/supabaseClient";
-import { ethers } from "ethers";
 import Layout from "@/components/Layout/Layout";
 
-declare let window: any;
-
 export default function Login() {
-  const [loginState, setLoginState] = useState("");
-  const [mataMaskAddress, setMataMaskAddress] = useState("");
-
-  const [userId, setUserId] = useState("");
-  const [userAddress, setUserAddress] = useState("");
-  const [token, setToken] = useState("");
-
-  const login = async () => {
-    setLoginState("matamask未接続");
-    // MateMaskがあるか？
-    if (!window?.ethereum) {
-      setLoginState("No MetaMask");
-      return;
-    }
-
-    // TODO 何やってるか後で見る
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-
-    // ウォレットの情報を取得
-    const signer = provider.getSigner();
-    // アドレス
-    const walletAddres = await signer.getAddress();
-    setLoginState("matamask接続成功");
-    setMataMaskAddress(walletAddres);
-    // レスポンス
-    const response = await fetch("/api/auth/nonce", {
-      method: "POST",
-      body: JSON.stringify({
-        walletAddres,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const { nonce } = await response.json();
-
-    // サインインメッセージ
-    const signature = await signer.signMessage(nonce);
-
-    // ユーザー情報取得
-    const userResponse = await fetch("/api/auth/wallet", {
-      method: "POST",
-      body: JSON.stringify({
-        walletAddress: walletAddres,
-        nonce,
-        signature,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log(userResponse);
-
-    const user = await userResponse.json();
-    console.log(user);
-
-    await supabase.auth.setAuth(user.token);
-    setToken(user.token);
-  };
-
-  const checkUser = async () => {
-    const { data, error } = await supabase.from("users").select("*");
-    console.log(data);
-    if (data) {
-      setUserId(data[0]?.id || "");
-      setUserAddress(data[0]?.wallet_address || "");
-    }
-  };
   return (
     <Layout>
-      <div className="container" style={{ padding: "50px 0 100px 0" }}>
-        <h1>matamask 接続状況 : {loginState}</h1>
-        <h1>matamask address : {mataMaskAddress}</h1>
-        <button onClick={login}>ログイン MataMask</button>
-        <hr />
-        <br />
-        <button onClick={checkUser}>チェックユーザー</button>
-        <h1>ユーザーID: {userId}</h1>
-        <h1>ユーザーアドレス: {userAddress}</h1>
-        <h1>トークン: {token}</h1>
-      </div>
+      <div className="container" style={{ padding: "50px 0 100px 0" }}></div>
     </Layout>
   );
 }
---
+++ src/components/Login/useMataMaskLogin.ts
@@ -0,0 +1,87 @@
+import { supabase } from "@/utils/supabaseClient";
+import { ethers } from "ethers";
+import { useState, useCallback } from "react";
+declare let window: any;
+
+type loginState = "Stay" | "Loading" | "Sucess" | "No MetaMask";
+
+export default function useMataMaskLogin() {
+  const [loginState, setLoginState] = useState<loginState>("Stay");
+  const [walletAddress, setWalletAddress] = useState("");
+
+  const getNonce = useCallback(async (walletAddres: string) => {
+    const response = await fetch("/api/auth/nonce", {
+      method: "POST",
+      body: JSON.stringify({
+        walletAddres,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { nonce } = await response.json();
+
+    return nonce;
+  }, []);
+
+  const getJstToken = useCallback(
+    async (walletAddres: string, nonce: string, signature: string) => {
+      const response = await fetch("/api/auth/wallet", {
+        method: "POST",
+        body: JSON.stringify({
+          walletAddress: walletAddres,
+          nonce,
+          signature,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const { token } = await response.json();
+
+      return token;
+    },
+    []
+  );
+
+  // TODO ドキュメント要確認
+  // TODO エラー処理
+  const MataMaskLogin = useCallback(async () => {
+    if (!window?.ethereum) {
+      setLoginState("No MetaMask");
+      return;
+    }
+
+    setLoginState("Loading");
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+
+    const signer = provider.getSigner();
+    const walletAddres = await signer.getAddress();
+    setWalletAddress(walletAddres);
+
+    const nonce = await getNonce(walletAddres);
+
+    const signature = await signer.signMessage(nonce);
+
+    const token = await getJstToken(walletAddres, nonce, signature);
+    await supabase.auth.setAuth(token);
+    setLoginState("Sucess");
+  }, [setLoginState, setWalletAddress, getNonce, getJstToken]);
+
+  const getLoginUser = useCallback(async () => {
+    const { data } = await supabase.from("users").select("*");
+    if (data) {
+      return data;
+    }
+    return null;
+  }, []);
+
+  return {
+    loginState,
+    walletAddress,
+    MataMaskLogin,
+    getLoginUser,
+  };
+}

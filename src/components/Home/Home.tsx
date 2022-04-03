01a336a4b1658a0f8bd100a79e0b18c4e89a51b2
aokisinn <72698168+aokisinn@users.noreply.github.com>
2022-04-04 00:24:17 +0900

    ボタンまで配置

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,12 @@ import { Box, Grid, TextField, Typography } from "@mui/material";
 import Layout from "../Layout/Layout";
 import MonthList from "./MonthList";
 import { useCallback, useState } from "react";
-import { LocalizationProvider, TimePicker } from "@mui/lab";
+import {
+  DesktopDatePicker,
+  LoadingButton,
+  LocalizationProvider,
+  TimePicker,
+} from "@mui/lab";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 
 const Home = () => {
@@ -30,34 +35,83 @@ const Home = () => {
         <Grid item xs={12} md={9}>
           <Box>
             <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <TimePicker
-                label="開始時刻"
-                value={value}
-                onChange={(newValue) => {
-                  setValue(newValue);
+              <div
+                style={{
+                  display: "inline",
+                  padding: "10px",
                 }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <TimePicker
-                label="休憩時刻"
-                value={value}
-                onChange={(newValue) => {
-                  setValue(newValue);
+              >
+                <DesktopDatePicker
+                  label="入力日"
+                  inputFormat="MM/dd/yyyy"
+                  value={null}
+                  onChange={(value) => console.log(value)}
+                  renderInput={(params) => <TextField {...params} />}
+                  disabled
+                />
+              </div>
+              <div
+                style={{
+                  display: "inline",
+                  padding: "10px",
                 }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <TimePicker
-                label="終了時刻"
-                value={value}
-                onChange={(newValue) => {
-                  setValue(newValue);
+              >
+                <TimePicker
+                  label="開始時刻"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </div>
+              <div
+                style={{
+                  display: "inline",
+                  padding: "10px",
                 }}
-                renderInput={(params) => <TextField {...params} />}
-              />
+              >
+                <TimePicker
+                  label="休憩時刻"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </div>
+              <div
+                style={{
+                  display: "inline",
+                  padding: "10px",
+                }}
+              >
+                <TimePicker
+                  label="終了時刻"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </div>
+              <div
+                style={{
+                  display: "inline",
+                  padding: "10px",
+                }}
+              >
+                <LoadingButton
+                  variant="contained"
+                  size="large"
+                  onClick={() => console.log("het")}
+                  sx={{
+                    margin: "5px",
+                  }}
+                >
+                  登録
+                </LoadingButton>
+              </div>
             </LocalizationProvider>
           </Box>
         </Grid>
